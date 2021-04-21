4a6dc44616f90d9090c3375a551b5229bdc7ca5e
ZHTushar23 <zahidhassantushar@gmail.com>
2021-04-21 17:18:08 +0200

    added new fc layer to improve classification

--- openselfsup/models/heads/__init__.py
+++ openselfsup/models/heads/__init__.py
@@ -3,3 +3,4 @@ from .cls_head import ClsHead
 from .latent_pred_head import LatentPredictHead
 from .multi_cls_head import MultiClsHead
 from .multi_cls_classification_head import MultiClsClassificationHead
+from .modified_cls_head import ModifiedClsHead
